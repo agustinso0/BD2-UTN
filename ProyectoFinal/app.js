44b20d3fda657a91d58c63ac2230629217492f06
Mateo Muscolino <muscolinomateo@gmail.com>
2025-07-02 18:59:28 -0300

    conexion con mongoose y node

---
+++ ProyectoFinal/app.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const { Producto, Movimiento, Proveedor } = require('./models');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const MONGODB_URL = process.env.MONGODB_URL;
+app.use(express.json());
+
+
+
+mongoose.connect(MONGODB_URL)
+  .then(() => console.log('Conectado a MongoDB'))
+  .catch((err) => console.error('Error al conectar a MongoDB:', err));
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
